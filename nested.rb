75b4ba031bda0fe08e70be333ac95ad6e1920831
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-09-30 01:06:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,11 +63,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
-	programmer_hash[0]=:yukihiro_matsumoto => {
-   :known_for => "Ruby",
-    :languages => ["LISP", "C"]
- }
+    programmer_hash=
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -83,7 +79,7 @@ def adding_matz
         }
      }
 
-    programmer_hash
+    programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
