5b9cdc660fe229e8e059094920596d70a70d63d2
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-09-30 00:24:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -14,9 +14,9 @@ def hopper
           :languages => ["C"]
         }
      }
-
+hopper[programmer_hash][:known_for]
 end
-hopper[:known_for]
+
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
